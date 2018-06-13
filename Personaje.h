b9806189d044d379ddbb298c6c56342b67c40c70
CarloValdivia <38506395+CarloValdivia@users.noreply.github.com>
2018-06-13 11:16:06 -0500

    arreglado origin y rotate

--- Personaje.h
+++ Personaje.h
@@ -4,14 +4,19 @@
 using namespace sf;
 using namespace std;
 
-class Juego;
+//clase abstracta Juego
+class Juego{
+public:
+    virtual actualizar(float){}
+    virtual Sprite* get_sprite(){}
+};
 
 class Personaje{
 public:
   Personaje();
-  void actualizar(float tiemp);
+  void actualizar(float);
   //Sprite get_sprite(){return *spr_personaje;};
-  Sprite* get_sprite(){return spr_personaje;};
+  Sprite* get_sprite(){return spr_personaje;}
 private:
   Sprite *spr_personaje;
   Texture *txt_personaje;

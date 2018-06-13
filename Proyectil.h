89fdb74b88bb556a313b89f172f0ac3d651fcbc3
CarloValdivia <38506395+CarloValdivia@users.noreply.github.com>
2018-06-13 10:08:59 -0500

    posicion, origen y rotacion

--- Proyectil.h
+++ Proyectil.h
@@ -36,9 +36,16 @@ void Proyectil::actualizar(float tiemp){
   tiemp/=10;
   velocidad.x+=aceleracion.x *tiemp;
   velocidad.y+=aceleracion.y *tiemp;
-  spr_proyectil->setPosition(spr_proyectil->getPosition().x+velocidad.x*tiemp,spr_proyectil->getPosition().y+velocidad.y*tiemp);
-  // borrar este IF para que solo se dibuje una vez
+  //spr_proyectil->setPosition(spr_proyectil->getPosition().x+velocidad.x*tiemp,spr_proyectil->getPosition().y+velocidad.y*tiemp);
+  //acomodando posicion
+  spr_proyectil->setPosition((spr_proyectil->getPosition().x /2.f)+velocidad.x*tiemp,(spr_proyectil->getPosition().y /2.f)+velocidad.y*tiemp);
 
+  //acomodando origen de la imagen a su centro
+  spr_proyectil->setOrigin(apr_proyectil->gettexture()->getSize().x /2.f, apr_proyectil->gettexture()->getSize().y /2.f);
+  //rotar proyectil
+  spr_proyectil->setRotation(spr_proyectil->getRotation+10);
+
+  // borrar este IF para que solo se dibuje una vez
 /*  if(spr_proyectil->getPosition().x> 500 && spr_proyectil-> getPosition().y >400 ){
 
     velocidad.x=50;
